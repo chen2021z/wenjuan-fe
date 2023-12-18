4429f8ddb43d500d98f8d1a98ea6b3497ad14a0a
Chen <2934570984@qq.com>
2023-12-19 00:04:51 +0800

    feat: 优化MainLaout布局

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-const MainLayout:React.FC = ()=> {
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import style from "./MainLayout.module.scss";
+const { Header, Content, Footer } = Layout;
+const MainLayout: React.FC = () => {
   return (
-    <>
-    <div>MainLayout header</div>
-    <div>
-      <Outlet></Outlet>
-    </div>
-    <div>MainLayout footer</div>
-    </>
-  )
-}
+    <Layout>
+      <Header className={style.heaader}>
+        <div className={style.left}>
+          logo
+        </div>
+        <div className={style.right}>
+          login
+        </div>
+      </Header>
+      <Layout  className={style.main}>
+        <Content>
+          <Outlet></Outlet>
+        </Content>
+      </Layout>
+      <Footer className={style.footer}>小治问卷 &copy;2023 - present. Created by 如约而至</Footer>
+    </Layout>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
