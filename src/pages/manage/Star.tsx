215d09ff06077b904607f4de04032344cc62251e
Chen <2934570984@qq.com>
2024-01-02 09:53:47 +0800

    feat：初步完成星标文件列表

--- src/pages/manage/Star.tsx
+++ src/pages/manage/Star.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
-const Star:React.FC = ()=> {
+import React, { useState } from 'react'
+import styles from './common.module.scss'
+import QuestionCard from '../../components/QuestionCard'
+import { useTitle } from 'ahooks'
+import { Typography, Empty } from 'antd'
+
+const { Title } = Typography
+const rawQuestionList = [
+  {
+    _id: 'q1',
+    title: '问卷1',
+    isPublished: false,
+    isStar: true,
+    answerCount: 5,
+    createdAt: '2023-12-14',
+  },
+  {
+    _id: 'q2',
+    title: '问卷2',
+    isPublished: true,
+    isStar: true,
+    answerCount: 5,
+    createdAt: '2023-12-14',
+  }
+]
+
+const Star: React.FC = () => {
+  useTitle('星标问卷')
+  const [questionList, setQuestionList] = useState(rawQuestionList)
   return (
-    <div>
-      Star
-    </div>
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title level={3}>星标问卷</Title>
+        </div>
+        <div className={styles.content}>搜索</div>
+      </div>
+      <div className={styles.content}>
+        {questionList.length === 0 && <Empty description="暂无数据" />}
+        {questionList.length > 0 &&
+          questionList.map(q => {
+            const { _id } = q
+            return <QuestionCard key={_id} {...q}></QuestionCard>
+          })}
+      </div>
+      <div className={styles.footer}>分页</div>
+    </>
   )
 }
 
