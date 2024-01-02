67980ea3bbd9800c10393f74399023e7df4faa59
Chen <2934570984@qq.com>
2024-01-02 09:38:25 +0800

    style：修正ManageList样式

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import styles from "./List.module.scss";
-import QuestionCard from "../../components/QuestionCard";
-import { useTitle } from "ahooks";
+import React, { useState } from 'react'
+import styles from './common.module.scss'
+import QuestionCard from '../../components/QuestionCard'
+import { useTitle } from 'ahooks'
 const rawQuestionList = [
   {
-    _id: "q1",
-    title: "问卷1",
+    _id: 'q1',
+    title: '问卷1',
     isPublished: false,
     isStar: false,
     answerCount: 5,
-    createdAt: "2023-12-14",
+    createdAt: '2023-12-14',
   },
   {
-    _id: "q2",
-    title: "问卷2",
-    isPublished: false,
-    isStar: false,
+    _id: 'q2',
+    title: '问卷2',
+    isPublished: true,
+    isStar: true,
     answerCount: 5,
-    createdAt: "2023-12-14",
+    createdAt: '2023-12-14',
   },
   {
-    _id: "q3",
-    title: "问卷3",
+    _id: 'q3',
+    title: '问卷3',
     isPublished: false,
     isStar: false,
     answerCount: 5,
-    createdAt: "2023-12-14",
+    createdAt: '2023-12-14',
   },
-];
+]
 
 const List: React.FC = () => {
   useTitle('我的问卷')
-  const [questionList, setQuestionList] = useState(rawQuestionList);
+  const [questionList, setQuestionList] = useState(rawQuestionList)
   return (
     <>
       <div className={styles.header}>
@@ -41,14 +41,16 @@ const List: React.FC = () => {
         <div className={styles.content}>搜索</div>
       </div>
       <div className={styles.content}>
-        {questionList.map((q) => {
-          const { _id } = q;
-          return <QuestionCard key={_id} {...q}></QuestionCard>;
-        })}
+        {/* 问卷列表 */}
+        {questionList.length > 0 &&
+          questionList.map(q => {
+            const { _id } = q
+            return <QuestionCard key={_id} {...q}></QuestionCard>
+          })}
       </div>
-      <div className={styles.footer}>footer</div>
+      <div className={styles.footer}>loadMore 上划加载更多...</div>
     </>
-  );
-};
+  )
+}
 
-export default List;
+export default List
