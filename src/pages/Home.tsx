04d8ed34df2b9943f81b56abb31db3577c55c45a
Chen <2934570984@qq.com>
2024-01-02 08:58:53 +0800

    feat：新增导航首页

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-const Home:React.FC = ()=> {
+import { Button, Typography } from 'antd'
+import styles from './Home.module.scss'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+
+const { Title, Paragraph } = Typography
+
+const Home: React.FC = () => {
   const nav = useNavigate()
-  function clickHandler(){
-    nav('/login')
-  }
+
   return (
-    <div>
-      Hmoe
-      <div>
-        <button onClick={clickHandler}>go login</button>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建文件 100 份，发布问卷 90 份，收到答卷 980 份</Paragraph>
+        <div>
+          <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+            开始使用
+          </Button>
+        </div>
       </div>
     </div>
   )
