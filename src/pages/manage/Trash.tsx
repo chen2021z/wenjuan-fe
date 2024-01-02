9b07cb575dc4de84cc7201ea73bafcd6aedc95c7
Chen <2934570984@qq.com>
2024-01-02 10:36:04 +0800

    feat: 初步完成回收站

--- src/pages/manage/Trash.tsx
+++ src/pages/manage/Trash.tsx
@@ -1,11 +1,121 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-const Home:React.FC = ()=> {
+import React, { FC, useState } from 'react'
+import { useTitle } from 'ahooks'
+import { Typography, Empty, Table, Tag, Button, Space, Modal, Spin, message } from 'antd'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
+import styles from './common.module.scss'
+
+const { Title } = Typography
+const { confirm } = Modal
+
+const rawQuestionList = [
+  {
+    _id: 'q1',
+    title: '问卷1',
+    isPublished: false,
+    isStar: true,
+    answerCount: 5,
+    createdAt: '2023-12-14',
+  },
+  {
+    _id: 'q2',
+    title: '问卷2',
+    isPublished: true,
+    isStar: true,
+    answerCount: 5,
+    createdAt: '2023-12-14',
+  },
+]
+
+const Trash: FC = () => {
+  useTitle('回收站')
+  const [questionList, setQuestionList] = useState(rawQuestionList)
+
+  // 记录选中的 id
+  const [selectedIds, setSelectedIds] = useState<string[]>([])
+
+  // 恢复
+
+  // 删除
+
+  function del() {
+    confirm({
+      title: '确认彻底删除该问卷？',
+      icon: <ExclamationCircleOutlined />,
+      content: '删除以后不可以找回',
+      onOk: () => alert('删除' + JSON.stringify(selectedIds)),
+    })
+  }
+
+  const tableColumns = [
+    {
+      title: '标题',
+      dataIndex: 'title',
+      // key: 'title', // 循环列的 key ，它会默认取 dataIndex 的值
+    },
+    {
+      title: '是否发布',
+      dataIndex: 'isPublished',
+      render: (isPublished: boolean) => {
+        return isPublished ? <Tag color="processing">已发布</Tag> : <Tag>未发布</Tag>
+      },
+    },
+    {
+      title: '答卷',
+      dataIndex: 'answerCount',
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createdAt',
+    },
+  ]
+
+  // 可以把 JSX 片段定义为一个变量
+  const TableElem = (
+    <>
+      <div style={{ marginBottom: '16px' }}>
+        <Space>
+          <Button type="primary" disabled={selectedIds.length === 0}>
+            恢复
+          </Button>
+          <Button danger disabled={selectedIds.length === 0} onClick={del}>
+            彻底删除
+          </Button>
+        </Space>
+      </div>
+      <div style={{ border: '1px solid #e8e8e8' }}>
+        <Table
+          dataSource={questionList}
+          columns={tableColumns}
+          pagination={false}
+          rowKey={q => q._id}
+          rowSelection={{
+            type: 'checkbox',
+            onChange: selectedRowKeys => {
+              setSelectedIds(selectedRowKeys as string[])
+            },
+          }}
+        />
+      </div>
+    </>
+  )
+
   return (
-    <div>
-      Hmoe
-    </div>
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title level={3}>回收站</Title>
+        </div>
+        <div className={styles.right}>{/* <ListSearch /> */}</div>
+      </div>
+      <div className={styles.content}>
+        {questionList.length === 0 && <Empty description="暂无数据" />}
+        {questionList.length > 0 && TableElem}
+      </div>
+      {/* <div className={styles.footer}>
+        <ListPage total={total} />
+      </div> */}
+    </>
   )
 }
 
-export default Home
+export default Trash
