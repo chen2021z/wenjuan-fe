c0d061a96ef536a8d33434aff7072a028dd85c36
Chen <2934570984@qq.com>
2024-01-02 08:16:25 +0800

    feat：新增UserInfo

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { Layout } from "antd";
-import style from "./MainLayout.module.scss";
-import Logo from "../components/Logo";
-const { Header, Content, Footer } = Layout;
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+import style from './MainLayout.module.scss'
+import Logo from '../components/Logo'
+import UserInfo from '../components/UserInfo'
+
+const { Header, Content, Footer } = Layout
 const MainLayout: React.FC = () => {
   return (
     <Layout>
@@ -12,17 +14,17 @@ const MainLayout: React.FC = () => {
           <Logo></Logo>
         </div>
         <div className={style.right}>
-          login
+          <UserInfo></UserInfo>
         </div>
       </Header>
-      <Layout  className={style.main}>
+      <Layout className={style.main}>
         <Content>
           <Outlet></Outlet>
         </Content>
       </Layout>
       <Footer className={style.footer}>小治问卷 &copy;2023 - present. Created by 如约而至</Footer>
     </Layout>
-  );
-};
+  )
+}
 
-export default MainLayout;
+export default MainLayout
