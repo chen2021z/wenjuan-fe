eaee4aeab3204d6ad13485b2bf2da0d21bde171f
Chen <2934570984@qq.com>
2024-01-02 08:59:03 +0800

    feat：新增404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
-const NotFound:React.FC = ()=> {
+import { Button, Result } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+import { useNavigate } from 'react-router-dom'
+
+const NotFound: React.FC = () => {
+  const nav = useNavigate()
+
   return (
-    <div>
-      NotFound
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，您访问的页面不存在"
+      extra={
+        <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+          返回首页
+        </Button>
+      }
+    />
   )
 }
 
