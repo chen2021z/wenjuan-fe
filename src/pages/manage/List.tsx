46f9277a423afb918f48c6ca8ca16a2a06c8c4f5
Chen <2934570984@qq.com>
2024-04-18 20:22:20 +0800

    feat: 使用useRequest 获取问卷列表

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,41 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './common.module.scss'
 import QuestionCard from '../../components/QuestionCard'
-import { useTitle } from 'ahooks'
-import { Typography } from 'antd'
+import { useRequest, useTitle } from 'ahooks'
+import { Spin, Typography } from 'antd'
 import ListSearch from '../../components/ListSearch'
+import { getQuestionListService } from '../../services/question'
 
 const { Title } = Typography
-const rawQuestionList = [
-  {
-    _id: 'q1',
-    title: '问卷1',
-    isPublished: false,
-    isStar: false,
-    answerCount: 5,
-    createdAt: '2023-12-14',
-  },
-  {
-    _id: 'q2',
-    title: '问卷2',
-    isPublished: true,
-    isStar: true,
-    answerCount: 5,
-    createdAt: '2023-12-14',
-  },
-  {
-    _id: 'q3',
-    title: '问卷3',
-    isPublished: false,
-    isStar: false,
-    answerCount: 5,
-    createdAt: '2023-12-14',
-  },
-]
 
 const List: React.FC = () => {
   useTitle('我的问卷')
-  const [questionList, setQuestionList] = useState(rawQuestionList)
+  const { data = {}, loading } = useRequest(getQuestionListService)
+  const { list: questionList = [], total = 0 } = data
+
   return (
     <>
       <div className={styles.header}>
@@ -47,9 +24,15 @@ const List: React.FC = () => {
         </div>
       </div>
       <div className={styles.content}>
+        {loading && (
+          <div style={{ textAlign: 'center' }}>
+            <Spin />
+          </div>
+        )}
         {/* 问卷列表 */}
-        {questionList.length > 0 &&
-          questionList.map(q => {
+        {!loading &&
+          questionList.length > 0 &&
+          questionList.map((q: any) => {
             const { _id } = q
             return <QuestionCard key={_id} {...q}></QuestionCard>
           })}
