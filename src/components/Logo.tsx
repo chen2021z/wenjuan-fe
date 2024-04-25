fe5df2bce5e8961378868031fd5cf6818d9c4b68
Chen <2934570984@qq.com>
2024-04-25 15:53:52 +0800

    feat: 使用redux 管理用户信息

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,20 +2,30 @@ import React, { FC, useEffect, useState } from 'react'
 import { Space, Typography } from 'antd'
 import { FormOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
+import useGetUserInfo from '../hooks/useGetUserInfo'
+import { HOME_PATHNAME, MANAGE_INDEX_PATHNAME } from '../router/index'
 import styles from './Logo.module.scss'
-import { MANAGE_INDEX_PATHNAME } from '../router'
 
 const { Title } = Typography
 
 const Logo: FC = () => {
+  const { username } = useGetUserInfo()
+
+  const [pathname, setPathname] = useState(HOME_PATHNAME)
+  useEffect(() => {
+    if (username) {
+      setPathname(MANAGE_INDEX_PATHNAME)
+    }
+  }, [username])
+
   return (
     <div className={styles.container}>
-      <Link to={MANAGE_INDEX_PATHNAME}>
+      <Link to={pathname}>
         <Space>
           <Title>
             <FormOutlined />
           </Title>
-          <Title>小治问卷</Title>
+          <Title>小慕问卷</Title>
         </Space>
       </Link>
     </div>
---
+++ src/store/userReducer.ts
@@ -0,0 +1,26 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+export type UserStateType = {
+  username: string
+  nickname: string
+}
+
+const INIT_STATE: UserStateType = {
+  username: '',
+  nickname: '',
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: INIT_STATE,
+  reducers: {
+    loginReducer: (state: UserStateType, action: PayloadAction<UserStateType>) => {
+      return action.payload // 设置 username nickname 到 redux store
+    },
+    logoutReducer: () => INIT_STATE,
+  },
+})
+
+export const { loginReducer, logoutReducer } = userSlice.actions
+
+export default userSlice.reducer
