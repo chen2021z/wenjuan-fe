0f5a66374798cf9c65b57e06a400e61808a27392
Chen <2934570984@qq.com>
2024-04-27 14:51:48 +0800

    feat: 将组件分组展示到组件库中

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
 import styles from './index.module.scss'
 import EditCanvas from './EditCanvas'
+import LeftPanel from './LeftPanel'
 
 const Edit: React.FC = () => {
   const { loading } = useLoadQuestionData()
@@ -11,11 +12,13 @@ const Edit: React.FC = () => {
       <div style={{ backgroundColor: '#fff' }}>Header</div>
       <div className={styles['content-wrapper']}>
         <div className={styles.content}>
-          <div className={styles.left}>Left</div>
+          <div className={styles.left}>
+            <LeftPanel />
+          </div>
           <div className={styles.main}>
             <div className={styles['canvas-wrapper']}>
               <EditCanvas loading={loading} />
-            </div> 
+            </div>
           </div>
           <div className={styles.right}>Right</div>
         </div>
